286b8cdb682b4d20c169939249ed3e5dd050768e
arhamumar94 <arhamumar1999@gmail.com>
2021-04-26 16:04:12 +0530

    hey

---
+++ src/components/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Backdrop.css'
+
+export const Backdrop = ({show,click}) => {
+    return show&&<div className="backdrop" onClick={click}></div>
+    
+}
---
+++ src/components/Product.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Product.css"
+import {Link} from 'react-router-dom';
+
+export const Product = ({ imageUrl, description, price, name, productId }) => {
+    return (
+        <div className="product">
+            <img src={imageUrl} alt={name}></img>
+            <div className="product__info">
+                <p className="info__name">{name}</p>
+                <p className="info__description">
+                {description.substring(0, 100)}
+                </p>
+                <p className="info__price">${price}</p>
+                <Link to={`/product/${productId}`} className="info__button">View</Link>
+            </div>
+            
+        </div>
+    )
+}
